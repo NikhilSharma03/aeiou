1d3cfbef1d18ff98beeed885e86f6d1d3717dab3
Nikhil Sharma <nikhilsharmamusic2000@gmail.com>
2022-06-19 19:43:15 +0530

    added success modal

---
+++ frontend/src/components/Modals/Success/success.style.ts
@@ -0,0 +1,76 @@
+import styled from 'styled-components';
+
+export const SuccessModalContainer = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 2000;
+`;
+
+export const SuccessModalOverlay = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100vh;
+    background-color: rgba(15, 15, 15, 0.95);
+`;
+
+export const SuccessModalBody = styled.div`
+    z-index: 2001;
+    background-color: ${({ theme }) => theme.palette.body};
+    box-shadow: 0 5px 15px rgba(0, 0, 0, 0.3);
+    border-radius: 3px;
+    overflow: hidden;
+    width: 60rem;
+    animation: pop-in 0.4s;
+    transition: all 0.3s;
+
+    @keyframes pop-in {
+        0% {
+            opacity: 0;
+            transform: scale(0.5);
+        }
+        100% {
+            opacity: 1;
+            transform: scale(1);
+        }
+    }
+`;
+
+export const SuccessModalHeader = styled.h1`
+    background-color: ${({ theme }) => theme.palette.scrollBarHover};
+    padding: 2rem;
+`;
+
+export const SuccessModalContent = styled.p`
+    padding: 5rem 2rem;
+    font-size: 1.6rem;
+    color: ${({ theme }) => theme.palette.headerTitle};
+`;
+
+export const SuccessButtonContainer = styled.div`
+    padding: 2rem;
+    display: flex;
+    justify-content: flex-end;
+`;
+
+export const SuccessCloseButton = styled.div`
+    background-color: ${({ theme }) => theme.palette.scrollBarHover};
+    padding: 1rem 3rem;
+    cursor: pointer;
+    margin-right: 2rem;
+    font-size: 1.25rem;
+    border-radius: 3px;
+    transition: all 0.3s;
+    font-weight: bold;
+
+    &:hover {
+        background-color: ${({ theme }) => theme.palette.scrollBar};
+    }
+`;
--- frontend/src/pages/NewCampaign/newcampaign.tsx
+++ frontend/src/pages/NewCampaign/newcampaign.tsx
@@ -18,6 +18,7 @@ import {
 import { useAppDispatch, useAppSelector } from './../../hooks/hooks';
 import LoadingModal from '../../components/Modals/Loading/loading';
 import ErrorModal from '../../components/Modals/Error/error';
+import SuccessModal from '../../components/Modals/Success/success';
 
 const NewCampaign: React.FC = () => {
     const [name, setName] = useState<string>('');
@@ -25,6 +26,7 @@ const NewCampaign: React.FC = () => {
     const [imageUrl, setImageUrl] = useState<string>('');
     const [minimumContribution, setMinimumContribution] = useState<string>('');
     const [targetAmount, setTargetAmount] = useState<string>('');
+    const [successMsg, setSuccessMsg] = useState<string>('');
     const dispatch = useAppDispatch();
     const userWalletAccount = useAppSelector(
         (state) => state.users.userWalletAccount
@@ -37,6 +39,7 @@ const NewCampaign: React.FC = () => {
         description,
         imageURL,
         minimumContribution,
+        targetAmt,
         userWalletAccount
     ) =>
         dispatch(
@@ -46,21 +49,22 @@ const NewCampaign: React.FC = () => {
                 imageURL,
                 minimumContribution,
                 userWalletAccount,
-                targetAmount,
+                targetAmount: targetAmt,
             })
         );
 
     const clearError = () => dispatch(onClearContractError());
     const setError = (msg: string) => dispatch(onSetContractError(msg));
 
-    const onSubmitHandler = (e) => {
+    const onSubmitHandler = async (e) => {
         e.preventDefault();
         // Validation
         if (
             name.trim().length === 0 ||
             description.trim().length === 0 ||
             imageUrl.trim().length === 0 ||
-            minimumContribution.trim().length === 0
+            minimumContribution.trim().length === 0 ||
+            targetAmount.trim().length === 0
         ) {
             // Set Error
             setError('Invalid Input!!');
@@ -68,11 +72,19 @@ const NewCampaign: React.FC = () => {
         }
 
         let minContri;
+        let targetAmt;
 
         try {
             minContri = web3.utils.toWei(minimumContribution);
         } catch (err) {
-            setError('Please enter amount in number!');
+            setError('Please enter amount in number for minimum Contribution!');
+            return;
+        }
+
+        try {
+            targetAmt = web3.utils.toWei(targetAmount);
+        } catch (err) {
+            setError('Please enter amount in number for target Amount!');
             return;
         }
 
@@ -86,18 +98,25 @@ const NewCampaign: React.FC = () => {
             description,
             imageUrl,
             minimumContribution: minContri,
+            targetAmount: targetAmt,
         };
-        createNewContract(
+        await createNewContract(
             data.name,
             data.description,
             data.imageUrl,
             data.minimumContribution,
+            data.targetAmount,
             userWalletAccount
         );
-        setName('');
-        setDescription('');
-        setImageUrl('');
-        setMinimumContribution('');
+
+        if (!error) {
+            setName('');
+            setDescription('');
+            setImageUrl('');
+            setMinimumContribution('');
+            setTargetAmount('');
+            setSuccessMsg('Campaign Created Successfully!');
+        }
     };
 
     return (
@@ -107,6 +126,11 @@ const NewCampaign: React.FC = () => {
                 showModal={!!error}
                 closeModal={clearError}
             />
+            <SuccessModal
+                content={successMsg}
+                showModal={!!successMsg}
+                closeModal={() => setSuccessMsg('')}
+            />
             <LoadingModal showModal={loading} />
             <FormHeader>
                 <FormHeaderTitle>New Campaign</FormHeaderTitle>
@@ -115,21 +139,25 @@ const NewCampaign: React.FC = () => {
                 <TextInput
                     type="text"
                     placeholder="Name"
+                    value={name}
                     onChange={(e) => setName(e.target.value)}
                 />
                 <TextInput
                     type="text"
                     placeholder="Description"
+                    value={description}
                     onChange={(e) => setDescription(e.target.value)}
                 />
                 <TextInput
                     type="text"
                     placeholder="ImageURL"
+                    value={imageUrl}
                     onChange={(e) => setImageUrl(e.target.value)}
                 />
                 <NCDiv>
                     <TextInput
                         type="text"
+                        value={minimumContribution}
                         placeholder="Minimum Contribution"
                         onChange={(e) => setMinimumContribution(e.target.value)}
                     />
@@ -138,6 +166,7 @@ const NewCampaign: React.FC = () => {
                 <NCDiv>
                     <TextInput
                         type="text"
+                        value={targetAmount}
                         placeholder="Target Amount"
                         onChange={(e) => setTargetAmount(e.target.value)}
                     />

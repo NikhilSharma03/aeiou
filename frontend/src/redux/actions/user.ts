e769a8489d6659cde36bf585e34de5f6ae78e145
Nikhil Sharma <nikhilsharmamusic2000@gmail.com>
2022-10-22 12:16:51 +0530

    added use goerli network

--- frontend/src/redux/actions/user.ts
+++ frontend/src/redux/actions/user.ts
@@ -2,6 +2,11 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import web3Modal from '../../utils/web3modal';
 import Web3 from 'web3';
 
+export const toHex = (num) => {
+    const val = Number(num);
+    return '0x' + val.toString(16);
+};
+
 export const onConnectWallet = createAsyncThunk<
     { account: string; web3: Web3 },
     void,
@@ -11,6 +16,10 @@ export const onConnectWallet = createAsyncThunk<
         localStorage.clear();
         const provider = await web3Modal.connect();
         const web3 = new Web3(provider);
+        provider.request({
+            method: 'wallet_switchEthereumChain',
+            params: [{ chainId: toHex(5) }],
+        });
         const accounts = await web3.eth.getAccounts();
         return { web3, account: accounts[0] };
     } catch (err) {
